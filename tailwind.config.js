0838af69e93e5291f5e6a6fa0cad54b5fec9e48a
Chris <cschinner7@gmail.com>
2025-01-15 21:26:41 -0800

    uodated tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,11 @@ export default {
     theme: {
         extend: {
             colors: {
-                "custom-color": "#ff6347", // Custom color you can use throughout
+                "custom-color": "#ff6347", // Custom color
                 "custom-background": "#ff6347", // Custom background color
             },
             fontFamily: {
-                custom: ['"Roboto"', "sans-serif"], // Adding a custom font
+                custom: ['"Roboto"', "sans-serif"],
             },
         },
     },
@@ -24,7 +24,7 @@ export default {
                     secondary: "#00aaff", // Custom secondary color
                     accent: "#34d399", // Custom accent color
                     neutral: "#2a2a2a", // Neutral background color
-                    "base-100": "#f8f8f8", // Background for your base color (main sections)
+                    "base-100": "#f8f8f8", // Background for base color (main sections)
                     info: "#2196F3", // Info color
                     success: "#4CAF50", // Success color
                     warning: "#ff9800", // Warning color
