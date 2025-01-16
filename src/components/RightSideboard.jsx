00eb0685d2c7e7047abb86ee704d7562a38e2c23
Chris <cschinner7@gmail.com>
2025-01-16 10:49:05 -0800

    update right sideboard

--- src/components/RightSideboard.jsx
+++ src/components/RightSideboard.jsx
@@ -4,12 +4,15 @@ const RightSideboard = () => {
     return (
         <div className="h-full w-full overflow-y-auto overflow-x-hidden">
             <div className="mb-2">
+                <p>Your search results...</p>
                 <SongCarousel />
             </div>
             <div className="mb-2">
+                <p>Top Picks!</p>
                 <SongCarousel />
             </div>
             <div className="mb-2">
+                <p>New Releases.</p>
                 <SongCarousel />
             </div>
         </div>
