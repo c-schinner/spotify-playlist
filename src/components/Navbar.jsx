b9b1656c07e9c70681943e2a6f46023ff8277c9e
Chris <cschinner7@gmail.com>
2025-01-22 11:48:12 -0800

    trying to get my firebase/google sign in to work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
 import { ImProfile } from "react-icons/im";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../FirebaseConfig";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const Navbar = () => {
+    const [user] = useAuthState(auth);
+
+    const googleSignIn = async () => {
+        const provider = new GoogleAuthProvider();
+        try {
+            await signInWithPopup(auth, provider);
+        } catch (error) {
+            console.errorr("Google Sign-In Error:", error.message);
+        }
+    };
+
+    const signOut = () => {
+        auth.signOut();
+    };
+
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost text-sm">Create your Playlists</a>
+                <p className="text-sm">Create your Playlists</p>
             </div>
             <div className="flex-none gap-2">
                 <div className="join">
@@ -39,19 +57,38 @@ const Navbar = () => {
                         className="btn btn-ghost btn-circle avatar"
                     >
                         <div className="w-10 rounded-full">
-                            <ImProfile size={42} />
+                            {user ? (
+                                <img src={user.photoURL} alt="Profile" />
+                            ) : (
+                                <ImProfile size={42} />
+                            )}
                         </div>
                     </div>
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
                     >
-                        <li>
-                            <a className="justify-between">Profile</a>
-                        </li>
-                        <li>
-                            <a>Logout</a>
-                        </li>
+                        {user ? (
+                            <li>
+                                <button
+                                    onClick={signOut}
+                                    className=""
+                                    type="button"
+                                >
+                                    Sign Out
+                                </button>
+                            </li>
+                        ) : (
+                            <li>
+                                <button
+                                    onClick={googleSignIn}
+                                    className=""
+                                    type="button"
+                                >
+                                    Sign In
+                                </button>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
