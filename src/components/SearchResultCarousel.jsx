e4805fdc26256b09783fe170cf3f728e8431f38f
Chris <cschinner7@gmail.com>
2025-01-27 10:19:36 -0800

    removing unnecessary comments

--- src/components/SearchResultCarousel.jsx
+++ src/components/SearchResultCarousel.jsx
@@ -41,6 +41,4 @@ SearchResultCarousel.propTypes = {
     data: PropTypes.array,
 };
 
-// Fix the layout for the info(band name and song  name) after searching
-
 export default SearchResultCarousel;
